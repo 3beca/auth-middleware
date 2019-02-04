95bc29ed22b6506d9da8f51162ead521725492c2
Francesco Rivola <francesco.rivola@gmail.com>
2019-02-04 21:52:30 +0100

    added example express-app

---
+++ example/express-app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import authMiddleware from "../src/index";
+
+const app = express();
+
+app.get("/", authMiddleware(), function handleRequest(req, res) {
+    res.json(req.user);
+});
+
+const port = process.NODE_ENV || 3000;
+app.listen(port, function() {
+    console.log(`Server listening at port ${port}`);
+})
